e88e5385f36b2905e66a2ef4e997059dfa620063
junekyu02 <baekjunekyu02@gmail.com>
2026-07-08 22:55:31 +0900

    init: 프로젝트 초기 세팅 및 PR/이슈 템플릿 추가

---
+++ .github/ISSUE_TEMPLATE/chore.md
@@ -0,0 +1,10 @@
+---
+name: 기타 작업
+about: 설정, 빌드, 의존성 등 작은 작업
+title: "[CHORE] "
+labels: "\U0001F3B5 CHORE"
+assignees: ''
+---
+
+## 🔧 작업 내용과 이유
+<!-- 무엇을, 왜 -->
---
+++ src/main/java/com/moamap/MoamapApplication.java
@@ -0,0 +1,12 @@
+package com.moamap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoamapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoamapApplication.class, args);
+    }
+}
