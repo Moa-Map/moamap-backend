c882c1252bb18495ec32a38e0d4f7212aaf368df
junekyu02 <baekjunekyu02@gmail.com>
2026-07-08 23:18:20 +0900

    refactor: MSA 멀티모듈 구조로 프로젝트 분리 (common/gateway/user/place/map/notification)

---
+++ map-service/src/test/java/com/moamap/map/MapServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.moamap.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
---
+++ place-service/src/test/java/com/moamap/place/PlaceServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.moamap.place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
