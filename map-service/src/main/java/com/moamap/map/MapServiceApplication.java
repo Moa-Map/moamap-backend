0f06274dcea439cf5ee2302282b2e4676441595b
kim-min-seo <picture0707@naver.com>
2026-07-09 16:48:12 +0900

    feat: 서울시 유동인구 데이터 배치 동기화 및 조회 API 구현 (#7)

--- map-service/src/main/java/com/moamap/map/MapServiceApplication.java
+++ map-service/src/main/java/com/moamap/map/MapServiceApplication.java
@@ -1,8 +1,13 @@
 package com.moamap.map;
 
+import com.moamap.map.client.SeoulOpenApiProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableConfigurationProperties(SeoulOpenApiProperties.class)
 @SpringBootApplication
 public class MapServiceApplication {
 
